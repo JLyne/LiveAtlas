63a799486ab1d866f00a8adab9bd8328612c0dd9
James Lyne <jim+github@not-null.co.uk>
2020-11-20 21:47:56 +0000

    Typescript

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
-  <Map />
-  <Sidebar />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import Sidebar from "@/components/Sidebar";
-import Map from "@/components/Map";
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    Map,
-    Sidebar,
-  }
-}
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
-<style>
-
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
