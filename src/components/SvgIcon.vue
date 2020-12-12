7070353b32197a939a728037bbcff0869864bde8
James Lyne <jim+github@not-null.co.uk>
2020-12-12 21:35:55 +0000

    SVG icon prerequisites

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg :class="className" xmlns="http://www.w3.org/2000/svg">
+    <title v-if="title">{{ title }}</title>
+    <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'svg-icon',
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    title: {
+      type: String,
+      default: null
+    }
+  },
+
+  computed: {
+    iconPath() {
+      let icon = require(`@/assets/icons/${this.name}.svg`);
+      if (Object.prototype.hasOwnProperty.call(icon, 'default')) {
+        icon = icon.default;
+      }
+
+      return icon.url;
+    },
+
+    className() {
+      return 'svg-icon svg-icon--' + this.name;
+    }
+  }
+};
+</script>
+
+<style>
+  .svg-icon {
+    fill: currentColor;
+    height: 24px;
+    width: 24px;
+  }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: '',
+	publicPath: '',
+
+	chainWebpack: config => {
+		config.module
+			.rule('svg-sprite')
+			.use('svgo-loader')
+			.loader('svgo-loader')
+	}
 }
