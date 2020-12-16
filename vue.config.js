c5c8bb366ac6352ed7c87f7f926e7cf6cb7cc789
James Lyne <jim+github@not-null.co.uk>
2020-12-16 22:41:24 +0000

    Don't minify index.html

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ module.exports = {
 	publicPath: '',
 
 	chainWebpack: config => {
+		config.plugin('html')
+			.tap(args => {
+				args[0].minify = false
+				return args
+			})
 		config.module
 			.rule('svg-sprite')
 			.use('svgo-loader')
