539e1db931586ded1b8fe348fa3ad5492d925aab
James Lyne <jim+github@not-null.co.uk>
2021-05-24 16:43:13 +0100

    Add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
-import svgSpritePlugin from "vite-plugin-svg-sprite-component"
+import svgSpritePlugin from "vite-plugin-svg-sprite-component";
+import analyze from 'rollup-plugin-analyzer';
 
 export default defineConfig({
-  plugins: [vue(), svgSpritePlugin({
+  plugins: [vue(), analyze(), svgSpritePlugin({
     symbolId: (name) => name,
   })],
   base: './',
