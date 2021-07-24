e4e7dfcd5feef1758291bff1bfa4501bf92d0306
James Lyne <jim+github@not-null.co.uk>
2021-07-24 20:13:19 +0100

    Move fetchJSON to abstract MapProvider

--- src/providers/MapProvider.ts
+++ src/providers/MapProvider.ts
@@ -16,10 +16,44 @@ export default abstract class MapProvider implements LiveAtlasMapProvider {
 		});
 	}
 
-	abstract destroy(): void;
 	abstract loadServerConfiguration(): Promise<void>;
 	abstract loadWorldConfiguration(world: LiveAtlasWorldDefinition): Promise<void>;
 	abstract sendChatMessage(message: string): void;
 	abstract startUpdates(): void;
 	abstract stopUpdates(): void;
+	abstract destroy(): void;
+
+	protected static async fetchJSON(url: string, signal: AbortSignal) {
+		let response, json;
+
+		try {
+			response = await fetch(url, {signal});
+		} catch(e) {
+			if(e instanceof DOMException && e.name === 'AbortError') {
+				console.warn(`Request aborted (${url}`);
+				throw e;
+			} else {
+				console.error(e);
+			}
+
+			throw new Error(`Network request failed`);
+		}
+
+		if (!response.ok) {
+			throw new Error(`Network request failed (${response.statusText || 'Unknown'})`);
+		}
+
+		try {
+			json = await response.json();
+		} catch(e) {
+			if(e instanceof DOMException && e.name === 'AbortError') {
+				console.warn(`Request aborted (${url}`);
+				throw e;
+			} else {
+				throw new Error('Request returned invalid json');
+			}
+		}
+
+		return json;
+	}
 }
