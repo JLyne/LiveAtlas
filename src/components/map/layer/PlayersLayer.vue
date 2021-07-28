ee8607ed6d34e50060a0a82575444f6b0c87b0cd
James Lyne <jim+github@not-null.co.uk>
2021-07-29 00:34:18 +0100

    Fix player layer duplicating in some situations

--- src/components/map/layer/PlayersLayer.vue
+++ src/components/map/layer/PlayersLayer.vue
@@ -77,7 +77,7 @@ export default defineComponent({
 	},
 
 	unmounted() {
-		this.leaflet.removeLayer(this.layerGroup);
+		this.leaflet.getLayerManager().removeLayer(this.layerGroup);
 	},
 
 	render() {
